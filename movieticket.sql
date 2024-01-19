d8bdc727251e8071f0a4c7dd1d6a6211aac8a1d3
ZO0NIC <zonicmohit@gmail.com>
2024-01-19 16:09:45 +0530

    Initial commit

---
+++ movieticket.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2021 at 10:03 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `movieticket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  `pay` int(11) NOT NULL,
+  `balance` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `subtotal`, `pay`, `balance`) VALUES
+(1, 300, 500, 200),
+(2, 900, 1000, 100),
+(3, 650, 1000, 350),
+(4, 650, 1000, 350),
+(5, 650, 1000, 350),
+(6, 1050, 1100, 50),
+(7, 650, 1000, 350),
+(8, 300, 500, 200),
+(9, 650, 700, 50),
+(10, 650, 1000, 350),
+(11, 1050, 1200, 150),
+(12, 1050, 2000, 950);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_product`
+--
+
+CREATE TABLE `sales_product` (
+  `id` int(11) NOT NULL,
+  `sales_id` int(11) NOT NULL,
+  `tickettype` varchar(255) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales_product`
+--
+ALTER TABLE `sales_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `sales_product`
+--
+ALTER TABLE `sales_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
